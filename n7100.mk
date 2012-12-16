05fb6dfed109f4d2096a186d3b7bf7c7b2afb506
codeworkx <codeworkx@cyanogenmod.org>
2012-12-16 16:03:07 +0100

    n7100: move uevent conf to common
    Change-Id: Ie88412bbd6901caeea067449fa2353f7325f53b0

--- n7100.mk
+++ n7100.mk
@@ -32,9 +32,7 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 # Init files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fstab.smdk4x12:root/fstab.smdk4x12 \
-    $(LOCAL_PATH)/init.smdk4x12.rc:root/init.smdk4x12.rc \
-    $(LOCAL_PATH)/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc \
-    $(LOCAL_PATH)/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc
+    $(LOCAL_PATH)/init.smdk4x12.rc:root/init.smdk4x12.rc
 
 # Audio
 PRODUCT_COPY_FILES += \
