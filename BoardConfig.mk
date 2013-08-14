7470ce8703de8eec98c9b89777947aa65e5b3045
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-08-14 20:02:51 +0200

    n7100: add selinux policies
    port from i9300: http://review.cyanogenmod.org/#/c/47915/5
    
    Change-Id: If232f942449b6a01a178cca36a5bc47fa6e4c8d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,23 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/n7100/bluetooth
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := cyanogenmod_n7100_defconfig
 
+# Selinux
+BOARD_SEPOLICY_DIRS := \
+    device/samsung/n7100/selinux
+
+BOARD_SEPOLICY_UNION := \
+    device.te \
+    domain.te \
+    file.te \
+    file_contexts \
+    init.te \
+    mediaserver.te \
+    rild.te \
+    system.te \
+    ueventd.te \
+    vold.te \
+    wpa_supplicant.te
+
 # assert
 TARGET_OTA_ASSERT_DEVICE := t03g,n7100,GT-N7100
 
