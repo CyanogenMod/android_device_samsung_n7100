f845fa380749ea6bf9f02084d80c069ae37ad4ce
myko <myko.hkg@gmail.com>
2013-09-06 11:27:09 +0800

    Enable BLE
    Change-Id: Ica4410448a3d561ddee64099f642a6ba2ae6bc48

--- n7100.mk
+++ n7100.mk
@@ -90,6 +90,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 
