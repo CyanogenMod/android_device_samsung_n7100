5eab7c660b8057c27b2c04d24844d167f43fcf47
Brandon Clothier <brandon14125@gmail.com>
2013-12-12 15:34:38 -0500

    n7100: opensource sensors This uses the patch from sbrissen with his opensource sensors.
    Change-Id: Ic6774e9d1c27f05b35e5a1c61b05384a99a88bac

--- n7100.mk
+++ n7100.mk
@@ -52,6 +52,10 @@ PRODUCT_PACKAGES += \
     libsecril-client-sap \
     SamsungServiceMode
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.smdk4x12
+
 # NFC
 PRODUCT_PACKAGES += \
     nfc.exynos4 \
