e0d420b122c1c5cf1772a362404669dbe9bb816d
markcs <mcampbellsmith@gmail.com>
2014-03-22 19:22:23 +0530

    n7100: audio: fix video issues
    Now audio is stereo without any static
    
    Change-Id: I69c4c66f9a8c91d1d005baf3bd79c3a32a868ac3

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -598,6 +598,18 @@ static void select_mode(struct m0_audio_device *adev)
             adev->in_call = 0;
             ril_set_call_clock_sync(&adev->ril, SOUND_CLOCK_STOP);
             end_call(adev);
+            //Force Input Standby
+            adev->in_device = AUDIO_DEVICE_NONE;
+
+            ALOGD("%s: set voicecall route: voicecall_default_disable", __func__);
+            set_bigroute_by_array(adev->mixer, voicecall_default_disable, 1);
+            ALOGD("%s: set voicecall route: default_input_disable", __func__);
+            set_bigroute_by_array(adev->mixer, default_input_disable, 1);
+            ALOGD("%s: set voicecall route: headset_input_disable", __func__);
+            set_bigroute_by_array(adev->mixer, headset_input_disable, 1);
+            ALOGD("%s: set voicecall route: bt_disable", __func__);
+            set_bigroute_by_array(adev->mixer, bt_disable, 1);
+
             force_all_standby(adev);
             select_output_device(adev);
             select_input_device(adev);
@@ -3081,7 +3093,7 @@ static int adev_open(const hw_module_t* module, const char* name,
     pthread_mutex_lock(&adev->lock);
     adev->mode = AUDIO_MODE_NORMAL;
     adev->out_device = AUDIO_DEVICE_OUT_SPEAKER;
-    adev->in_device = AUDIO_DEVICE_IN_BUILTIN_MIC & ~AUDIO_DEVICE_BIT_IN;
+    adev->in_device = AUDIO_DEVICE_NONE;
     select_devices(adev);
 
     adev->pcm_modem_dl = NULL;
