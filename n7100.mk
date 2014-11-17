8446116f0e83199213748ce3fc50b2d1b7e81274
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-11-17 07:42:35 +0530

    n7100: update mobile data interfaces
    Change-Id: Id12e56cbe08bdee0e83589ddce691bc01fe72646

--- n7100.mk
+++ n7100.mk
@@ -95,7 +95,7 @@ $(call inherit-product, vendor/pa/configs/nfc_enhanced.mk)
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungExynos4RIL \
-    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
+    mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1 \
     ro.telephony.call_ring.multiple=false \
     ro.telephony.call_ring.delay=3000
 
