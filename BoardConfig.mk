df7a0c97a31929b0850aab68c6f0ddae4f7adc29
Howard Su <howard0su@gmail.com>
2014-11-29 06:28:34 +0000

    n7100: enable legacy mmap

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,9 +54,14 @@ TARGET_OTA_ASSERT_DEVICE := t03g,n7100,GT-N7100
 # inherit from the proprietary version
 -include vendor/samsung/n7100/BoardConfigVendor.mk
 
+# Blobs
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/n7100/rootdir/fstab.smdk4x12
 RECOVERY_FSTAB_VERSION := 2
+
+BOARD_USES_LEGACY_MMAP := true
 
 # Camera wrapper
 TARGET_PROVIDES_CAMERA_HAL := true
