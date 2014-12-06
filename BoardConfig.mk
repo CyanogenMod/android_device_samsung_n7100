d6a42d4bc900b0faca81a018e9746b81c9afc4a2
Howard Su <howard0su@gmail.com>
2014-12-06 00:07:57 +0000

    n7100: fix build of selinux.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,9 +36,9 @@ BOARD_RIL_CLASS := ../../../device/samsung/n7100/ril/telephony/java
 
 # Selinux
 BOARD_SEPOLICY_DIRS := \
-    device/samsung/n7100/sepolicy
+    device/samsung/n7100/selinux
 
-#BOARD_SEPOLICY_UNION := \
+BOARD_SEPOLICY_UNION := \
     device.te \
     domain.te \
     file.te \
