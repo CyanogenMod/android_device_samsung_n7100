0892f48f2f454ec19517c5b57b5836974b4483c3
tilaksidduram <tilaksidduram@gmail.com>
2015-11-19 23:27:29 +0530

    n7100: remove selinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,20 +47,3 @@ RECOVERY_FSTAB_VERSION := 2
 
 # Compatibility with pre-kitkat Sensor HALs
 SENSORS_NEED_SETRATE_ON_ENABLE := true
-
-# Selinux
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/n7100/selinux
-
-BOARD_SEPOLICY_UNION += \
-    device.te \
-    domain.te \
-    file.te \
-    file_contexts \
-    init.te \
-    mediaserver.te \
-    rild.te \
-    system.te \
-    ueventd.te \
-    vold.te \
-    wpa_supplicant.te
