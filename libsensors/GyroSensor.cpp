3c24870455d00278748e7d9c1272449f2204da2f
tilaksidduram <tilaksidduram@gmail.com>
2016-04-13 21:31:01 +0530

    n7100: libsensors: actually try enabling before checking if it worked

--- libsensors/GyroSensor.cpp
+++ libsensors/GyroSensor.cpp
@@ -80,19 +80,16 @@ int GyroSensor::setInitialState() {
 
 int GyroSensor::enable(int32_t handle, int en) {
     int flags = en ? 1 : 0;
-    int fd;
+    int err;
     if (flags != mEnabled) {
-        strcpy(&input_sysfs_path[input_sysfs_path_len], "enable");
-        fd = open(input_sysfs_path, O_RDWR);
-        if (fd >= 0){
-            write(fd, en == 1 ? "1" : "0", 2);
-            close(fd);
-            mEnabled = flags;
-            setInitialState();
+         err = sspEnable(LOGTAG, SSP_GYRO, en);
+         if(err >= 0){
+             mEnabled = flags;
+             setInitialState();
 
-            return 0;
-        }
-        return -1;
+             return 0;
+         }
+         return -1;
     }
     return 0;
 }
