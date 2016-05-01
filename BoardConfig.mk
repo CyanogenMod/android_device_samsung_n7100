90851bd3b7e94eb6be3e6b864e5956993633e88b
Trafalgar-Square <schneller.demian@gmail.com>
2016-05-01 14:27:00 +0200

    Revert "n7100: use samsung power hal"
    This reverts commit 2f50aff55cb13253b204f831adfd3ca0ddb347e0.
    
    Change-Id: I60308962f91ad991fd7bef82622a3f733bc150c1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,9 +39,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/n7100/bluetooth
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := cyanogenmod_n7100_defconfig
 
-# Power HAL
-TARGET_POWERHAL_VARIANT := samsung
-
 # assert
 TARGET_OTA_ASSERT_DEVICE := t03g,n7100,GT-N7100
 
